f2d1deafac99e1b779a620c651c6addda8d298ae
Noah Syrkis <nobr@itu.dk>
2020-02-04 13:21:04 +0100

    add hello from Noah

---
+++ script.py
@@ -0,0 +1,12 @@
+# script.py
+#   simple print script
+# by: Group B
+
+def hello_world():
+    return 'hello group B, from Noah'
+
+def main():
+    print(hello_world())
+
+if __name__ == "__main__":
+    main() 
